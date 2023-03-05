363e59a01c2b4ba84d1bab20ae0f2a8cceefb273
Iliah Nazarov <48030498+meta110@users.noreply.github.com>
2023-03-05 15:22:38 +0300

    Update api2pbi.m

--- API-constructor/api2pbi.m
+++ API-constructor/api2pbi.m
@@ -1,3 +1,6 @@
+// генератор функций для работы с методами API
+// новая версия здесь: https://github.com/meta110/powerbi/blob/master/API-constructor/api2pbi.m
+// документация здесь: 
 each
 let
     args = _,
